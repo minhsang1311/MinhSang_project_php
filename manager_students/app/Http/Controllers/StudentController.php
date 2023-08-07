7b2a68d67c8a3ccb9605c10dc7db4d203521e254
minhsang1311 <109707462+minhsang1311@users.noreply.github.com>
2023-08-07 23:09:32 +0700

    nn

--- manager_students/app/Http/Controllers/StudentController.php
+++ manager_students/app/Http/Controllers/StudentController.php
@@ -14,9 +14,9 @@ class StudentController extends Controller
      */
     public function index()
     {
-        $data = Student::latest()->paginate(5);
+        $data = Student::latest()->paginate(10);
 
-        return view('index', compact('data'))->with('i', (request()->input('page', 1) - 1) * 5);
+        return view('index', compact('data'))->with('i', (request()->input('page', 1) - 1) * 10);
     }
 
     /**
